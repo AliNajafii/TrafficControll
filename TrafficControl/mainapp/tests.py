9a4f0dd4cf44d1a00b4e118a7c160a3730ddcd92
AliNajafii <58827763+AliNajafii@users.noreply.github.com>
2022-01-27 20:16:07 +0330

    Testing Car and Owner
    Passed

--- TrafficControl/mainapp/tests.py
+++ TrafficControl/mainapp/tests.py
@@ -1,3 +1,66 @@
 from django.test import TestCase
+from .models import Owner,Car
+from .models import CarTypes,ColorTypes
+from django.db.utils import OperationalError
 
-# Create your tests here.
+class OwnerModelTest(TestCase):
+    """
+        Note:
+        beacuse django test uses different
+        DB the designed Trigger in main DB
+        for checking the big car constraint
+        of each owner is ignored.
+        But i tested it in shell and it worked.
+        """ 
+    def setUp(self):
+        Owner.objects.all().delete()
+        Car.objects.all().delete()
+        self.owner1 = Owner(
+            name = 'o1',
+            age = 50,
+            national_code = 182764533,
+            total_toll_paid = None
+        )
+        self.owner1.save()
+
+        super().setUp()
+    
+    def test_adding_many_small_cars_to_owner(self):
+        self.owner2 = Owner.objects.create(
+            id = 7,
+            name = 'Ali',
+            age = 23,
+
+        )
+        car3 = Car.objects.create(
+            car_type = CarTypes.SMALL.value,
+            load_balance = None,
+            color = ColorTypes.RED.value,
+            owner = self.owner2
+        )
+        car4 = Car.objects.create(
+            car_type = CarTypes.SMALL.value,
+            load_balance = 200,
+            color = ColorTypes.BLUE.value,
+            owner = self.owner2
+        )
+        car5 = Car.objects.create(
+            car_type = CarTypes.SMALL.value,
+            load_balance = 100,
+            color = ColorTypes.GREEN.value,
+            owner = self.owner2
+        )
+
+        self.assertEqual(self.owner2.car_set.all().count(),3)
+        self.assertIn(car3,self.owner2.car_set.all())
+
+
+    
+        
+        
+        
+        
+        
+
+        
+
