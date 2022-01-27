6013c0739fbae2f729467f0ac0d99740a5bbb826
AliNajafii <58827763+AliNajafii@users.noreply.github.com>
2022-01-27 22:48:37 +0330

    testing AbstractBaseBackend

---
+++ TrafficControl/nosql_management/tests.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from .Rmodels import AbstractBaseBackEnd,NoSQLDBs
+from .exceptions import NotModule
+import inspect
+import redis
+class TestAbstractbaseBackend(TestCase):
+
+    def setUp(self):
+        self.backend = AbstractBaseBackEnd(redis,NoSQLDBs.REDIS)
+    
+    def test_start(self):
+        
+        connection = self.backend.start()
+        self.assertIsInstance(connection,self.backend.connector)
+        backends_functions_itself = inspect.getmembers(
+            self.backend,
+            inspect.isfunction
+        )
+        backends_functions_itself = [code for _,code in backends_functions_itself]
+        self.assertIn(redis.Redis.geoadd,backends_functions_itself)
+        self.assertIn(redis.Redis.save,backends_functions_itself)
+        self.assertIn(redis.Redis.sadd,backends_functions_itself)
+        self.assertIn(redis.Redis.lpush,backends_functions_itself)
+    
+    def test_set_api_module(self):
+        with self.assertRaises(NotModule):
+            self.backend._set_api_module('This is not module')
+    
+    def test_find_possible_connectors(self):
+        connector = self.backend._find_possible_connectors()
+        self.assertEqual(connector,redis.Redis)
+        
