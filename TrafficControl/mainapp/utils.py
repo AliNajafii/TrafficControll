af3a8004b2e8d07c56897720dfcfbd9da18ce313
AliNajafii <58827763+AliNajafii@users.noreply.github.com>
2022-01-28 21:20:54 +0330

    LimitedQueryMixin added

---
+++ TrafficControl/mainapp/utils.py
@@ -0,0 +1,17 @@
+class LimitedQueryMixin:
+    """
+    associate with model classes to 
+    limit query and increase performance.
+    """
+    @classmethod
+    def limit_query(cls,query:QuerySet,offset=0,percent=.2):
+        """
+        yeild query each time returns percent
+        of total items. if items are less than 10 
+        it returns query.
+        """
+        count = query.count()
+        if count > 10
+        step = count * percent
+        for limit in range(offset,count,step):
+            yield query[offset:limit]
