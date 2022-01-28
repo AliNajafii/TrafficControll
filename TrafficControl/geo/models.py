3cec47cce5e2d023e4eff5617af6930d7a03086b
AliNajafii <58827763+AliNajafii@users.noreply.github.com>
2022-01-28 18:49:59 +0330

    add road field for TollStation and added some functionality to Road

--- TrafficControl/geo/models.py
+++ TrafficControl/geo/models.py
@@ -1,4 +1,24 @@
 from django.db import models
+from .utils import make_line,in_line
+from django.db.models import QuerySet
+
+class LimitedQueryMixin:
+    """
+    associate with model classes to 
+    limit query and increase performance.
+    """
+    @classmethod
+    def limit_query(cls,query:QuerySet,offset=0,percent=.2):
+        """
+        yeild query each time returns percent
+        of total items. if items are less than 10 
+        it returns query.
+        """
+        count = query.count()
+        if count > 10
+        step = count * percent
+        for limit in range(offset,count,step):
+            yield query[offset:limit]
 
 class Position(models.Model):
     """
@@ -24,18 +44,59 @@ class AbstractBaseRoad(models.Model):
     This class has all features
     of a road. like name and width
     """
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30,null=True,blank=True)
     width = models.CharField(max_length=15)
     class Meta:
         abstract = True
 
-class Road(AbstractBaseRoad):
+class Road(AbstractBaseRoad,LimitedQueryMixin):
     """
     Each road has many routs
     """
 
     class Meta:
         unique_together = ['name','width']
+    
+
+    @staticmethod
+    def roads_with_toll_station(cls):
+        """
+        returns all roads id which have
+        toll stations in them.
+        """
+        all_toll_stations = TollStation.objects.all()
+        toll_stations_without_specified_road = set()
+        response_roads = set()
+        for t in all_toll_stations:
+            if not t.road :
+                toll_stations_without_specified_road.add(t)
+            else :
+                road = cls.objects.get(pk=t.road)
+                response_roads.add(road)
+
+            for toll in all_toll_stations :
+                if toll not in toll_stations_without_specified_road:
+                    all_roads = cls.objects.all()
+                    if not all_roads:
+                        return response_roads 
+                    for road_list in cls.limit_query(all_roads): #for better performance using limit
+                                                                    #see LimitedQueryMixin
+                        for road in road_list:
+                            if road.in_this_road(t.lat,t.lng):
+                                response_roads.add(road)
+                                t.road = road
+                                t.save()
+            
+    
+    def in_this_road(self,lat,lng):
+        """
+        returns True if a point is
+        in this road.
+        """
+        points = self.route_set.all().values_list('lat','lng')
+        line = make_line(*list(points))
+        return in_line(lat,lng,line)
+    
     
 class Route(Position):
     """Represents a path position
@@ -54,6 +115,15 @@ class TollStation(Position):
     """
     name = models.CharField(max_length=10)
     toll_per_cross = models.IntegerField()
-
+    per_kilogram_cost = models.IntegerField(default=300)
+    road = models.ForeignKey(
+        'Road',
+        on_delete = models.CASCADE,
+        null = True,
+        blank = True
+    ) #this field at first is null based on our 
+        #data set but for searching faster
+        #when querying to find a tollstation is located
+        # in which road , we can inisialize this field
     
     
