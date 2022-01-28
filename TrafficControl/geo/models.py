edfb7c9596a340e2938a6a03294867e79a3adddb
AliNajafii <58827763+AliNajafii@users.noreply.github.com>
2022-01-28 21:22:14 +0330

    LimitedQueryMixin removed and pushed in mainapp/utils

--- TrafficControl/geo/models.py
+++ TrafficControl/geo/models.py
@@ -1,24 +1,7 @@
 from django.db import models
 from .utils import make_line,in_line
 from django.db.models import QuerySet
-
-class LimitedQueryMixin:
-    """
-    associate with model classes to 
-    limit query and increase performance.
-    """
-    @classmethod
-    def limit_query(cls,query:QuerySet,offset=0,percent=.2):
-        """
-        yeild query each time returns percent
-        of total items. if items are less than 10 
-        it returns query.
-        """
-        count = query.count()
-        if count > 10
-        step = count * percent
-        for limit in range(offset,count,step):
-            yield query[offset:limit]
+from mainapp.utils import LimitedQueryMixin 
 
 class Position(models.Model):
     """
