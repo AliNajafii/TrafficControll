b6c8601a04ab7e99e93e3bc2adf4d941ed10410f
AliNajafii <58827763+AliNajafii@users.noreply.github.com>
2022-01-28 18:50:33 +0330

    add a few functionality to Car and Owner

--- TrafficControl/mainapp/models.py
+++ TrafficControl/mainapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Q
 from geo.models import Position
 from enum import Enum
 
@@ -39,6 +40,22 @@ class Owner(Person):
         blank=True
         )
     
+    @classmethod
+    def violated_owners(cls):
+        """
+        returns all owners which didnt
+        pay their Toll but passed from
+        them.
+        violated owners are passed the tollstations
+        but did not paied. it means total_toll_paid
+        is null or from when they tracked we excpect
+        that total_toll_paid will be more than the amount
+        we expected.
+        """
+    
+    
+    
+    
 class Car(models.Model):
     """Represents Car
     A class which has 
@@ -74,6 +91,22 @@ class Car(models.Model):
         on_delete = models.CASCADE,
         related_query_name= 'cars'
     )
+
+    def path_traveled(self,start_date=None,end_date=None):
+        """
+        return positions traveld by this car
+        betwwen start_date and end_date
+        """
+        if (start_date and not end_date) or (end_date and not start) :
+            raise ValueError('you should set both start_date and end_date')
+
+        if not start_date and not end_date:
+            return self.cartraffic_set.all().values_list('lat','lng')
+            
+        between_date = Q(date__range=(start_date,end_date))
+        points = self.cartraffic_set.all().filter(between_date).values_list('lat','lng')
+        return list(points)
+
 
 class CarTraffic(Position):
     """represants traffic of spesific car
