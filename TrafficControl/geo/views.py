c58ee8f3bfe616d4cb6070380108b470082958fc
AliNajafii <58827763+AliNajafii@users.noreply.github.com>
2022-01-30 17:18:14 +0330

    viewsets added

--- TrafficControl/geo/views.py
+++ TrafficControl/geo/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from.models import Road,TollStation
-from .sreializers import RoadModelSerializer
+from .sreializers import RoadModelSerializer,TollStationSerializer
 from rest_framework import viewsets
 
 class RoadAPIViewSet(viewsets.ModelViewSet):
     queryset = Road.objects.all()
     serializer_class = RoadModelSerializer
+    lookup_field = 'name'
+
+    def get_serializer(self, *args, **kwargs):
+        if isinstance(kwargs.get("data", {}), list):
+            kwargs["many"] = True
+        return super().get_serializer(*args,**kwargs)
+
+class TollStationAPIViewSet(viewsets.ModelViewSet):
+    queryset = TollStation.objects.all()
+    serializer_class = TollStationSerializer
+
+    def get_serializer(self, *args, **kwargs):
+        if isinstance(kwargs.get("data", {}), list):
+            kwargs["many"] = True
+        return super().get_serializer(*args,**kwargs)
