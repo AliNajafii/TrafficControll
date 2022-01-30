45d5818275ef9f3c8feb20495b33601a37cb6635
AliNajafii <58827763+AliNajafii@users.noreply.github.com>
2022-01-30 13:06:57 +0330

    Update sreializers.py

--- TrafficControl/geo/sreializers.py
+++ TrafficControl/geo/sreializers.py
@@ -3,13 +3,21 @@ from rest_framework import serializers
 
 
 #---Model Serializers
-class RouteModelSerializer(serializers.ModelSerializer)
+class RouteModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Route
+        fields = '__all__'
 
 class RoadModelSerializer(serializers.ModelSerializer):
     """
     Serializes The Road objects
     """
-    routs = serializers.HyperlinkedModelSerializer()
+    routes = RouteModelSerializer(many=True,label='path-points')
     class Meta :
         model = Road
-        fields = ('name')
+        fields = ('name','width','routs')
+
+class TollStationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TollStation
+        exclude = ('id',)
