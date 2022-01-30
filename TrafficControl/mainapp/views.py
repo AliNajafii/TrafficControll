663453cde91092e51a8ff9b538fd392da3900866
AliNajafii <58827763+AliNajafii@users.noreply.github.com>
2022-01-30 13:07:12 +0330

    model view sets added

--- TrafficControl/mainapp/views.py
+++ TrafficControl/mainapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Owner,Car
+from .serializers import OwnerModelSerializer,CarModelSerializer
 
-# Create your views here.
+class OwnerAPIViewSet(viewsets.ModelViewSet):
+    queryset = Owner.objects.all()
+    serializer_class = OwnerModelSerializer
+
+class CarAPIViewSet(viewsets.ModelViewSet):
+    queryset = Car.objects.all()
+    serializer_class = CarModelSerializer
