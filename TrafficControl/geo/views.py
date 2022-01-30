8dff73e34095a80477b5d684ff13859c6a43f75d
AliNajafii <58827763+AliNajafii@users.noreply.github.com>
2022-01-30 13:05:54 +0330

    View sets added

--- TrafficControl/geo/views.py
+++ TrafficControl/geo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from.models import Road,TollStation
+from .sreializers import RoadModelSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+class RoadAPIViewSet(viewsets.ModelViewSet):
+    queryset = Road.objects.all()
+    serializer_class = RoadModelSerializer
