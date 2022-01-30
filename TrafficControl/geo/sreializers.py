f576e7b1203705d58096884b1e7dc83186527ad2
AliNajafii <58827763+AliNajafii@users.noreply.github.com>
2022-01-30 17:17:55 +0330

    bulk create add for raods and toll stations

--- TrafficControl/geo/sreializers.py
+++ TrafficControl/geo/sreializers.py
@@ -1,30 +1,83 @@
 from .models import Road,Route,TollStation
 from rest_framework import serializers 
+from django.db.models import ObjectDoesNotExist
 
 
 #---Model Serializers
 class RouteModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Route
-        fields = '__all__'
+        fields = ('lat','lng',)
+
+class BulkRoadCreate(serializers.ListSerializer):
+    
+    """
+    this serializer is for bulk creation.
+    client can pass Road route point too.
+    """
+    def create(self,validated_data):
+        roads = []
+        routes = []
+        route_data = None
+        for data in validated_data:
+            try:
+                route_data = data.pop('route_set')
+            except KeyError:
+                pass
+            road = Road(**data)
+            roads.append(road)
+            if route_data:
+                routes.append(Route(road=road,**route_data))
+        roads = Road.objects.bulk_create(roads)
+        Route.objects.bulk_create(routes)
+        return roads
 
 class RoadModelSerializer(serializers.ModelSerializer):
     """
     Serializes The Road objects
     """
-    points = RouteModelSerializer(many=True)
+    route_set = RouteModelSerializer(many=True,required=False,label='points')
     class Meta :
         model = Road
-        fields = ('name','width','points',)
+        fields = ('name','width','route_set',)
+        list_serializer_class = BulkRoadCreate
     
     def create(self,validated_data):
-        points = validated_data.pop('points')
+        points = {}
+        try :
+            points = validated_data.pop('route_set')
+        except KeyError:
+            pass
         road = Road.objects.create(**validated_data)
         for point_data in points :
             Route.objects.create(road=road,**point_data)
         return road
+    
+    def update(self,instance,validated_data):
+        routes_data = None
+        try :
+            routes_data = validated_data.pop('route_set')
+        except KeyError:
+            pass
+        try:
+            road = Road.objects.get(id=instance.id)
+        except ObjectDoesNotExist:
+            raise serializers.ValidationError(
+        f'Road {instance.name} , {instance.width} does not exists.'
+        )
+        
+
+class BulkTollStationSerializer(serializers.ListSerializer):
+    """
+    this serializer is for creating bulk toll station
+    """
+    def create(self,validated_data):
+        stations = [TollStation(**data) for data in validated_data]
+        return TollStation.objects.bulk_create(stations) 
 
 class TollStationSerializer(serializers.ModelSerializer):
     class Meta:
         model = TollStation
         exclude = ('id',)
+    
+    
